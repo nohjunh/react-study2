143724a8bb63a344d611cd6b5b05839897eeecbe
nohjunh <nsh042777@naver.com>
2022-09-05 20:17:20 +0900

    sort 활용

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
 import FoodList from "./FoodList";
 import items from "../mock.json";
+import { useState } from "react";
 
 function App() {
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => {
+    if (order === "createdAt") {
+      // 문자열로 접근하므로 []대괄호 표기법으로 객체에 접근한다.
+      return a[order] - b[order];
+    } else {
+      return b[order] - a[order];
+    }
+  });
+
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleCalorieClick = () => setOrder("calorie");
+
   return (
     <div>
-      <FoodList items={items} />
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleCalorieClick}>칼로리순</button>
+      </div>
+      <FoodList items={sortedItems} />
     </div>
   );
 }
--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,18 +1,34 @@
+import "./FoodList.css";
+
+function formatDate(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
 function FoodListItem({ item }) {
-  const { imgUrl, title, calorie, content } = item;
+  const { imgUrl, title, calorie, content, createdAt } = item;
 
   return (
-    <div>
+    <div className="FoodListItem">
       <img src={imgUrl} alt={title} />
       <div>{title}</div>
       <div>{calorie}</div>
       <div>{content}</div>
+      <div>{formatDate(createdAt)}</div>
     </div>
   );
 }
 
 function FoodList({ items }) {
-  return <ul></ul>;
+  return (
+    <ul className="FoodList">
+      {items.map((item) => (
+        <li>
+          <FoodListItem item={item} />
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default FoodList;
